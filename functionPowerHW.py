e8286603ffd0afaae8e5eb38fca4c5c0b141be51
ethan <echung2018@pingry.org>
2016-03-07 22:53:58 -0500

    third commit. the program now accepts exponents that are either 0, positive, or negative

--- functionPowerHW.py
+++ functionPowerHW.py
@@ -1,13 +1,27 @@
 def power():
 	base = int(input("Give me a base "))
-	exponent = int(input("Give me an exponent ")) - 1 
+	exponent = int(input("Give me an exponent ")) 
 	#one was subtracted from the exponent variable before, so when the user uses exponent '1', the output is not the base * the base
 	#in other words, you have to account for the fact that 2^2 is not 2*2*2 but 2*2
 	
 	newNumber = base
-	while exponent > 0: #this loop ensures that the base undergoes the exponential operation for as many times as the exponent dictates
-		newNumber = base * newNumber
+	if exponent < 0:
+		exponent = exponent + 1
+		while exponent < 0:
+			newNumber = base * newNumber
+			exponent = exponent + 1
+		newNumber = 1/newNumber
+		
+	elif exponent > 0:
 		exponent = exponent - 1
+		while exponent > 0: #this loop ensures that the base undergoes the exponential operation for as many times as the exponent dictates
+			newNumber = base * newNumber
+			exponent = exponent - 1
+		
+	elif exponent == 0:
+		exponent = exponent - 1
+		newNumber = 1
+		
 	print(newNumber)
 
 power()
