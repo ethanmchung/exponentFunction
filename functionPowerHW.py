7a8fa3d9abb3f9d0e6193ef79d9e5f68b489f00b
ethan <echung2018@pingry.org>
2016-03-07 23:11:20 -0500

    fourth commit. comments were fixed and some unnecessary lines of code were removed

--- functionPowerHW.py
+++ functionPowerHW.py
@@ -1,10 +1,11 @@
 def power():
 	base = int(input("Give me a base "))
 	exponent = int(input("Give me an exponent ")) 
-	#one was subtracted from the exponent variable before, so when the user uses exponent '1', the output is not the base * the base
-	#in other words, you have to account for the fact that 2^2 is not 2*2*2 but 2*2
 	
 	newNumber = base
+	#one is subtracted from the exponent variable, so when the user uses positive exponent '1', the output is not the base * the base
+	#in other words, you have to account for the fact that 2^2 is not 2*2*2 but 2*2;
+	#the same applies for a negative exponent but in this case one is added to the negative exponent variable
 	if exponent < 0:
 		exponent = exponent + 1
 		while exponent < 0:
@@ -19,7 +20,6 @@ def power():
 			exponent = exponent - 1
 		
 	elif exponent == 0:
-		exponent = exponent - 1
 		newNumber = 1
 		
 	print(newNumber)
